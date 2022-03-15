b6fb50c51d82bb249fb638a5cfee50034c253ebc
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-03-15 19:06:10 +0000

    set New Pokemon

--- tests/ejercicio-1/pokedex.spec.ts
+++ tests/ejercicio-1/pokedex.spec.ts
@@ -6,6 +6,7 @@ import {Pokedex} from '../../src/ejercicio-1/pokedex';
 const Gyarados = new Pokemon('Gyarados', 6.5, 230, 'Water', 65, 60, 110, 500);
 const Chikorita = new Pokemon('Chikorita', 0.9, 6.4, 'Grass', 49, 65, 45, 45);
 const Ninetales = new Pokemon('Ninetales', 1.1, 19.9, 'Fire', 76, 75, 100, 73);
+const Voltorb = new Pokemon('Voltorb', 0.5, 10.4, 'Electric', 30, 50, 100, 40);
 
 const pokedex = new Pokedex([Gyarados, Chikorita, Ninetales]);
 
@@ -29,6 +30,13 @@ describe('Pokedex Class Tests', () => {
       const pokedexKanto = new Pokedex();
       pokedexKanto.setPokemonList([Gyarados, Ninetales]);
       expect(pokedexKanto.getPokemonList()).to.be.eql([Gyarados, Ninetales]);
+    });
+  });
+  describe('Pokedex new pokemon add list', () => {
+    it('Wild Voltorb Appeared!', () => {
+      pokedex.setNewPokemon(Voltorb);
+      expect(pokedex.getPokemonList())
+          .to.be.eql([[Gyarados, Chikorita, Ninetales, Voltorb]]);
     });
   });
 });
