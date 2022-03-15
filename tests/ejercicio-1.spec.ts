af113bf400270c75f21d145cc8eaac9e7814617d
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-03-15 17:42:40 +0000

    getname test method ready

--- tests/ejercicio-1.spec.ts
+++ tests/ejercicio-1.spec.ts
@@ -2,11 +2,13 @@ import 'mocha';
 import {expect} from 'chai';
 import {Pokemon} from '../src/ejercicio-1/pokemon';
 
+const Gyarados = new Pokemon('Gyarados', 6.5, 230, 'Water', 65, 60, 110, 500);
+const Chikorita = new Pokemon('Chikorita', 0.9, 6.4, 'Grass', 49, 65, 45, 45);
+const Ninetales = new Pokemon('Ninetales', 1.1, 19.9, 'Fire', 76, 75, 100, 73);
+
 describe('Pokemon class tests', () => {
   describe('Pokemon Attributes must be accessibles', () => {
     it('Example 1: Gyarados', () => {
-      const Gyarados =
-        new Pokemon('Gyarados', 6.5, 230, 'Water', 65, 60, 110, 500);
       expect(Gyarados.name).to.be.eql('Gyarados');
       expect(Gyarados.height).to.be.eql(6.5);
       expect(Gyarados.weight).to.be.eql(230);
@@ -15,6 +17,13 @@ describe('Pokemon class tests', () => {
       expect(Gyarados.defense).to.be.eql(60);
       expect(Gyarados.speed).to.be.eql(110);
       expect(Gyarados.hp).to.be.eql(500);
+    });
+  });
+  describe('Pokemon Class getters', () => {
+    it('getName method', () => {
+      expect(Gyarados.getName()).to.be.eql('Gyarados');
+      expect(Chikorita.getName()).to.be.eql('Chikorita');
+      expect(Ninetales.getName()).to.be.eql('Ninetales');
     });
   });
 });
