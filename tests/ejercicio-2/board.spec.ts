13ddaf9c4433edd0823e0f995ccc50342b2bf7ce
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-03-16 17:08:22 +0000

    test for board getters

--- tests/ejercicio-2/board.spec.ts
+++ tests/ejercicio-2/board.spec.ts
@@ -2,10 +2,26 @@ import 'mocha';
 import {expect} from 'chai';
 import {Board} from '../../src/ejercicio-2/board';
 
+const emptyBoard = new Board(6, 7);
+const defaultBoard = new Board();
+
 describe('Board Class tests', () => {
-  it('Board Constructor', () => {
-    const emptyBoard = new Board(6, 7);
-    expect(emptyBoard.rows).to.be.eql(6);
-    expect(emptyBoard.columns).to.be.eql(7);
+  describe('Constructor values tests', () => {
+    it('Board Constructor', () => {
+      expect(emptyBoard.rows).to.be.eql(6);
+      expect(emptyBoard.columns).to.be.eql(7);
+    });
+    it('Board Constructor Default values', () => {
+      expect(defaultBoard.rows).to.be.eql(6);
+      expect(defaultBoard.columns).to.be.eql(7);
+    });
+  });
+  describe('Board getters', () => {
+    it('getRows method', () => {
+      expect(defaultBoard.getRows()).to.be.eql(6);
+    });
+    it('getColumns method', () => {
+      expect(defaultBoard.getColumns()).to.be.eql(7);
+    });
   });
 });
