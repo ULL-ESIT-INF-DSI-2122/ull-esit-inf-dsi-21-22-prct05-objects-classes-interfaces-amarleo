ad952d19c6a5291de4cc513a753fcb7f7b747582
Alejandro Martín de León <alu01015941@ull.edu.es>
2022-03-17 09:43:02 +0000

    test pass for tostring and valueof methods

--- src/modificacion/modificacion.ts
+++ src/modificacion/modificacion.ts
@@ -4,4 +4,12 @@ export class Hexadecimal {
   constructor(decimal: number) {
     this.decimal = decimal;
   }
+
+  toString() {
+    return '0x' + this.decimal.toString(16);
+  }
+
+  valueOf() {
+    return this.decimal;
+  }
 }
