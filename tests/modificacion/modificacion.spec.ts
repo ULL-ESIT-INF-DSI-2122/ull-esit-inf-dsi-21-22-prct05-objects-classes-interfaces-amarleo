4930495ae3e0b64078d4540d1d30caf886953566
Alejandro Martín de León <alu01015941@ull.edu.es>
2022-03-17 09:40:07 +0000

    Hexadecimal methods tests ready

--- tests/modificacion/modificacion.spec.ts
+++ tests/modificacion/modificacion.spec.ts
@@ -8,4 +8,12 @@ describe('Hexadecimal Class tests', () => {
   it('Hexadecimal Constructor', () => {
     expect(myHexNumber.decimal).to.be.eql(38);
   });
+  describe('Hexadecimal methods', () => {
+    it('toString() method', () => {
+      expect(myHexNumber.toString()).to.be.eql('0x26');
+    });
+    it('valueOf method', () => {
+      expect(myHexNumber.valueOf()).to.be.eql(38);
+    })
+  });
 });
