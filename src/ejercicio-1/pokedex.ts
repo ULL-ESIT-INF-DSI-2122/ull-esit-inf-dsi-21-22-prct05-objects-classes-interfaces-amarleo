2c6bb084c3be0758fa7258adda960ad5a4875a4e
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-03-20 18:25:33 +0000

    pokedex class doc ready

--- src/ejercicio-1/pokedex.ts
+++ src/ejercicio-1/pokedex.ts
@@ -1,20 +1,36 @@
 import {Pokemon} from './pokemon';
 
+/**
+ * Pokedex Class
+ */
 export class Pokedex {
   pokemonList: Pokemon[];
 
+  /**
+   * Pokedex Class Constructor
+   * @param pokemonList Array of Pokemon objects
+   */
   constructor(pokemonList: Pokemon[] = []) {
     this.pokemonList = pokemonList;
   }
-
+  /**
+   * Pokemon array getter
+   * @returns array with all known pokemon
+   */
   getPokemonList() {
     return this.pokemonList;
   }
-
+  /**
+  *Pokemon Array setter
+  * @param pokemonList array with all known pokemons
+  */
   setPokemonList(pokemonList: Pokemon[]) {
     this.pokemonList = pokemonList;
   }
-
+  /**
+   * Pokemon Array Setter. It sets a new Pokemon on the Pokedex.
+   * @param newPokemon New Pokemon object
+   */
   setNewPokemon(newPokemon: Pokemon) {
     if (this.pokemonList.indexOf(newPokemon) === -1) {
       this.pokemonList.push(newPokemon);
